6b38ffa3bfcdf8ca740f3cdd464194a309c43a70
suraj <surajc@audaxlabs.com>
2024-04-13 22:45:43 +0530

    updated server.py

--- server.py
+++ server.py
@@ -1,9 +1,5 @@
 import uvicorn
 from app import app
-from db.model import UserModel, JournalModel
-from db import engine
 
 if __name__ == "__main__":
-    UserModel.metadata.create_all(engine)
-    JournalModel.metadata.create_all(engine)
     uvicorn.run(app, host="0.0.0.0", port=8181)
